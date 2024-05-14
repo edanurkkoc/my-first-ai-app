316733fd71f82d7f2b36d1c5bd0dff23dfebf09e
Edanur Kocakoc <125741593+edanurkkoc@users.noreply.github.com>
2024-05-14 17:47:40 +0300

    app demo çalışmam

---
+++ demo_streamlit.py
@@ -0,0 +1,22 @@
+import streamlit as st 
+from langchain.llms import OpenAI
+st.write("Az once yazdigim yapay zekama welcome buyurdunuz!")
+
+st.title("ERENOS")
+
+openai_api_key=st.sidebar.text_input("OpenAI API Key")
+
+def response(input_text):
+    llm=OpenAI(temperature=0.7,
+               openai_api_key=openai_api_key,)
+    st.info(llm(input_text))
+
+with st.form("my_form"):
+    text =st.text_area("Enter text",
+                       "Write three tips for learning NLP")
+    submitted=st.form_submit_button("Submit")
+    
+    if not openai_api_key.startswith("sk-"):
+        st.warning("Please enter your OpenAI API key!",icon="😸")
+    if submitted and openai_api_key.startswith("sk-"):
+        response(text)
